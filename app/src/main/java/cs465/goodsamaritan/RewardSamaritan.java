a340608329f8d7b2a59202241b29df3b808de5f3
Joe Salinas <jvsalin2@illinois.edu>
2016-04-26 22:11:50 -0500

    My commit 2

--- app/src/main/java/cs465/goodsamaritan/RewardSamaritan.java
+++ app/src/main/java/cs465/goodsamaritan/RewardSamaritan.java
@@ -2,6 +2,8 @@ package cs465.goodsamaritan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class RewardSamaritan extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class RewardSamaritan extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reward_samaritan);
+        Spinner dropdown = (Spinner)findViewById(R.id.spinner1);
+        String[] items = new String[]{"John Doe 2/15/16", "Mary Pots 3/2/16", "Jack Nichols  4/23/16"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+
     }
+
+
 }
+
+
+

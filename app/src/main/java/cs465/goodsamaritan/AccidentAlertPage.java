4f56dcd3e27ad878a5fb8fb445ed9fe17941ab0b
Joe Salinas <jvsalin2@illinois.edu>
2016-04-27 22:55:08 -0500

    Another commit

---
+++ app/src/main/java/cs465/goodsamaritan/AccidentAlertPage.java
@@ -0,0 +1,13 @@
+package cs465.goodsamaritan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccidentAlertPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accident_alert_page);
+    }
+}

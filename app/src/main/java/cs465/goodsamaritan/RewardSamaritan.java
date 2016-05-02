33e6dfce4aeebe904657c58abad14b6c9064f946
Joe Salinas <jvsalin2@illinois.edu>
2016-05-02 12:25:00 -0500

    Another commit

--- app/src/main/java/cs465/goodsamaritan/RewardSamaritan.java
+++ app/src/main/java/cs465/goodsamaritan/RewardSamaritan.java
@@ -12,7 +12,7 @@ public class RewardSamaritan extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reward_samaritan);
         Spinner dropdown = (Spinner)findViewById(R.id.spinner1);
-        String[] items = new String[]{"John Doe 2/15/16", "Mary Pots 3/2/16", "Jack Nichols  4/23/16"};
+        String[] items = new String[]{"Car accident: John Doe", "Drowning Accident: Mary Pots", "Heart Attack incident:Jack Nichols"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
         dropdown.setAdapter(adapter);
 
